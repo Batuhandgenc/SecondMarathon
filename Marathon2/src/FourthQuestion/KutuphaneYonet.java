8aa6df3c4321418fe0815111493f65fb89760e97
pc <pc@DESKTOP-PSR7FGD>
2022-08-08 13:33:03 +0300

    FourthQuestion

--- Marathon2/src/FourthQuestion/KutuphaneYonet.java
+++ Marathon2/src/FourthQuestion/KutuphaneYonet.java
@@ -20,6 +20,24 @@ public class KutuphaneYonet {
         kitaplar.add(yeniKitap);
     }
 
+    public class KitapAra {
+        private KutuphaneYonet dir;
+
+        public KitapAra(KutuphaneYonet tree) {
+            this.dir = tree;
+        }
+
+        public List<Kitap> search(String state) {
+            List<Kitap> results = new ArrayList<Kitap>();
+            for (Kitap a : dir.TumKitaplar()) {
+                if (a.getClass().getName().equals(state) || a.kitapBasligi().equals(state))
+                    results.add(a);
+            }
+            return results;
+       }
+
+    }
+    
     public boolean KitapKarsilastir(Kitap book) {
         return kitaplar.contains(book);
     }
@@ -59,22 +77,6 @@ public class KutuphaneYonet {
     public HashSet<KitapBasligi> TumKitapTurleri() {
         return kitapBasligi;
     }
-    public class KitapAra {
-        private KutuphaneYonet dir;
-
-        public KitapAra(KutuphaneYonet tree) {
-            this.dir = tree;
-        }
-
-        public List<Kitap> search(String state) {
-            List<Kitap> results = new ArrayList<Kitap>();
-            for (Kitap a : dir.TumKitaplar()) {
-                if (a.getClass().getName().equals(state) || a.kitapBasligi().equals(state))
-                    results.add(a);
-            }
-            return results;
-       }
-
-    }
+    
     
 }
